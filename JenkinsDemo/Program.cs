ab9e080ba94f1cb8cc15decd671d17501224fcd4
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2025-08-19 21:46:23 +0300

    Initialize Create

---
+++ JenkinsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey , JenkinsDemo");
+        }
+
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ JenkinsDemoUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿
+using JenkinsDemo;
+
+namespace JenkinsDemoUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            Assert.Equal(5, Program.Add(2, 3));
+        }
+    }
+}
